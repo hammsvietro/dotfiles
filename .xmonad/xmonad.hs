0156ed434a323d16b55b32434799fe8b5a0cff69
hammsvietro <hammsvietro@gmail.com>
2022-06-03 09:37:39 -0300

    changes wallpaper

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -289,7 +289,7 @@ myStartupHook = do
   spawnOnce "xrandr --output DVI-D-0 --off --output HDMI-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-0 --primary --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-1 --off &"
   spawnOnce "picom --experimental-backends &"
   spawnOnce "xscreensaver -no-splash &"
-  spawn "/usr/bin/feh --bg-fill ~/wallpapers/mountains.jpg &"
+  spawn "/usr/bin/feh --bg-fill ~/wallpapers/vim.jpg &"
   setWMName "LG3D"
 
 
