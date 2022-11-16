58a0e4e4f8e2b8e448dc14afcb90e6e89a1f8cd1
hammsvietro <hammsvietro@gmail.com>
2022-11-16 16:37:21 -0300

    remove experimental-backends flag when spawning picom

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -300,7 +300,7 @@ myHandleEventHook = dynamicPropertyChange "WM_NAME" $ composeAll
 -- By default, do nothing.
 myStartupHook = do
   spawnOnce "xrandr --output DVI-D-0 --off --output HDMI-0 --mode 2560x1080 --pos 1920x0 --rotate normal --output DP-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off &"
-  spawnOnce "picom --experimental-backends &"
+  spawnOnce "picom &"
   spawnOnce "xscreensaver -no-splash &"
   spawn "/usr/bin/feh --bg-fill ~/wallpapers/sunrise.jpg &"
   setWMName "LG3D"
