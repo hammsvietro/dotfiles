68dc5eb8afa90d22441c0e9d4b2768511746e733
hammsvietro <hammsvietro@gmail.com>
2023-01-19 16:44:49 -0300

    minor autoclose change

--- .config/nvim/lua/autoclose_setup.lua
+++ .config/nvim/lua/autoclose_setup.lua
@@ -3,9 +3,8 @@ local config = {
       ["("] = { escape = false, close = true, pair = "()"},
       ["["] = { escape = false, close = true, pair = "[]"},
       ["{"] = { escape = false, close = true, pair = "{}"},
-      ["<"] = { escape = false, close = true, pair = "<>"},
 
-      [">"] = { escape = true, close = false, pair = "<>"},
+      [">"] = { escape = true, close = true, pair = "<>"},
       [")"] = { escape = true, close = false, pair = "()"},
       ["]"] = { escape = true, close = false, pair = "[]"},
       ["}"] = { escape = true, close = false, pair = "{}"},
