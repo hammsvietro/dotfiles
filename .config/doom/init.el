167114e341054a2f122a87136ba6db90fbfcead8
hammsvietro <hammsvietro@gmail.com>
2025-02-06 15:03:13 -0300

    Sync

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -121,7 +121,7 @@
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       ;;data              ; config/data formats
+       data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
        (elixir +lsp +tree-sitter)            ; erlang done right
