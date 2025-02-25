8f6d5b1cd2006e6215bba876fc5676daf29e2369
Pedro Vietro <hammsvietro@gmail.com>
2025-02-25 19:36:01 -0300

    sync

--- .config/home-manager/modules/apps.nix
+++ .config/home-manager/modules/apps.nix
@@ -1,5 +1,25 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ spotify obsidian stremio discord pavucontrol ];
+  home.packages = with pkgs; [
+    spotify
+    stremio
+    discord
+    pavucontrol
+
+    # Google Chrome override (using `override` instead of `overrideAttrs`)
+    (google-chrome.override {
+      commandLineArgs =
+        "--enable-features=UseOzonePlatform --ozone-platform=wayland";
+    })
+
+    # Obsidian override (using `overrideAttrs`)
+    (obsidian.overrideAttrs (old: {
+      postFixup = (old.postFixup or "") + ''
+        wrapProgram $out/bin/obsidian \
+          --set NIXOS_OZONE_WL 1 \
+          --add-flags "--disable-gpu-compositing --enable-features=UseOzonePlatform --ozone-platform=wayland"
+      '';
+    }))
+  ];
 }
