9f194614fb71b2b56c63dcf78e8e6c8835ce4faa
Pedro Vietro <hammsvietro@gmail.com>
2026-03-27 09:19:29 -0300

    sync

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -3,7 +3,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-;; (setq org-directory "~/org/")
+(setq org-directory "~/org/")
+(setq org-roam-directory "~/org/roam/")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -154,7 +154,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +dragndrop +pandoc +pretty)                 ; organize your plain life in plain text
+       (org +dragndrop +pandoc +pretty +roam2)                 ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
