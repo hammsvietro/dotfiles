a2cf7415e2c4c74450f8cf31f75c9a97a93597e4
Pedro Vietro <hammsvietro@gmail.com>
2026-03-27 09:19:29 -0300

    sync

--- modules/base.nix
+++ modules/base.nix
@@ -51,6 +51,7 @@
     enableSSHSupport = true;
     pinentryPackage = pkgs.pinentry-qt;
   };
+  services.openssh.enable = true;
   environment.shellInit = ''
     gpg-connect-agent /bye
     export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
